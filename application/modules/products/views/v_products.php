58172682010b3963437205310b250f4e6a441e37
Adima Mesa <mesadima@gmail.com>
2015-02-09 17:20:21 +0300

    very minor commit

--- application/modules/products/views/v_products.php
+++ application/modules/products/views/v_products.php
@@ -190,12 +190,6 @@
 				<!--implementing from the db-->
 				
 				
-				<div id="cart">
-					
-					
-				</div>
-				
-				
 				<div class="col-sm-9 padding-right">
 					<div class="features_items"><!--features_items-->
 						<h2 class="title text-center">Features Items</h2>
